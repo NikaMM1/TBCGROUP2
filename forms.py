4d279b6821242a4b1fa83c884df6231bd10ee80e
NikaMM1 <nikamproff@gmail.com>
2024-07-08 21:47:23 +0400

    Add files via upload

--- forms.py
+++ forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField,SelectField, EmailField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Register')
 
 class LoginForm(FlaskForm):
@@ -14,12 +14,10 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class OrderForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired(), ])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     address = StringField('Address', validators=[DataRequired()])
-    food_item = SelectField('Food Item', choices=[('pizza', 'Pizza'), ('burger', 'Burger'), ('sushi', 'Sushi')], validators=[DataRequired()])
     card_number = StringField('Card Number', validators=[DataRequired()])
-    card_name = StringField('Name on Card', validators=[DataRequired()])
-    expiration_date = StringField('Expiration Date', validators=[DataRequired()])
+    card_name = StringField('Card Name', validators=[DataRequired()])
     cvv = StringField('CVV', validators=[DataRequired()])
-    submit = SubmitField('Submit')
-    
+    food_item = StringField('Food Item', validators=[DataRequired()])
+    submit = SubmitField('Submit Order')
--- models.py
+++ models.py
@@ -1,53 +1,52 @@
 from ext import db
 from flask_login import UserMixin
 
+def register_user(username, password):
+    new_user = User(username=username, password=password)  
+    db.session.add(new_user)
+    db.session.commit()
 
-class User(db.Model):
-    __tablename__ = 'users'
-
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, )
-    password_hash = db.Column(db.String(128), )
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
     is_admin = db.Column(db.Boolean, default=True)
 
-   
-    __tablename__ = 'orders'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), )
-    address = db.Column(db.String(150), )
-    food_item = db.Column(db.String(50),)
-    card_number = db.Column(db.String(20),)
-    card_name = db.Column(db.String(150), )
-    expiration_date = db.Column(db.String(10),)
-    cvv = db.Column(db.String(4), )
-    cart_items = db.relationship('OrderItem', backref='order', lazy=True)
+    orders = db.relationship('Order', backref='user')
 
 class MenuItem(db.Model):
     __tablename__ = 'menu_item'
+    
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128),)
+    name = db.Column(db.String(128), unique=True)
     description = db.Column(db.Text)
-    price = db.Column(db.Float,)
+    price = db.Column(db.Float)
     image_url = db.Column(db.String(256))
-    def __init__(self, name, description, price, image_url):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.image_url = image_url
+
+class Order(db.Model):
+    __tablename__ = 'orders'  
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120))
+    address = db.Column(db.String(200))
+    card_number = db.Column(db.String(16))
+    card_name = db.Column(db.String(100))
+    cvv = db.Column(db.String(4))
+    food_item = db.Column(db.String(100))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    order_items = db.relationship('OrderItem', backref='order', cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f'<Order {self.id}>'
+
 class OrderItem(db.Model):
     __tablename__ = 'order_items'  
-    id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'),)  
-    name = db.Column(db.String(128),)
-    description = db.Column(db.String(256),)
-    price = db.Column(db.Float, )
-    image_url = db.Column(db.String(256), )
+    
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(256))
+    price = db.Column(db.Float)
     
 
-
-def __init__(self, id, name, description, price, image_url):
-    self.id = id
-    self.name = name
-    self.description = description
-    self.price = price
-    self.image_url = image_url
